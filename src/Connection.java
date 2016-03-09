f64e3c9bc82d865aad8b2c1009c083d4725b2d7a
Thiago Costa Rizuti da Rocha <t.rizuti@gmail.com>
2016-03-09 11:04:29 -0300

    Files cleaning

--- src/Connection.java
+++ src/Connection.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package src;
 
 import java.io.DataInputStream;
@@ -14,10 +9,6 @@ import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author thiago
- */
 public class Connection implements Runnable {
    Socket clientSocket;
    int clientId;
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package src;
 
 import java.util.Scanner;
 import static javafx.scene.input.KeyCode.I;
 
-/**
- *
- * @author thiago
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]){
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter port to start server");
